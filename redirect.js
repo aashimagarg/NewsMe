1d805dd62d2eec6d4d778603b2359139804e8055
aashimagarg <agarg@utexas.edu>
2016-12-22 00:25:22 -0600

    test

--- redirect.js
+++ redirect.js
@@ -1,12 +1,16 @@
 
 // get the active tab url
 chrome.tabs.getCurrent(function(tab){
+		if (tab.url.search("facebook") != -1) {
+			var newURL = "http://nytimes.com/";
+ 			chrome.tabs.create({ url: newURL });
+		}
         console.log(tab.url);
     }
 );
 
-// temporary - when the extension icon is clicked, launch NYTimes
-chrome.browserAction.onClicked.addListener(function(activeTab){
-  var newURL = "http://nytimes.com/";
-  chrome.tabs.create({ url: newURL });
-});
+// // temporary - when the extension icon is clicked, launch NYTimes
+// chrome.browserAction.onClicked.addListener(function(activeTab){
+//   var newURL = "http://nytimes.com/";
+//   chrome.tabs.create({ url: newURL });
+// });
