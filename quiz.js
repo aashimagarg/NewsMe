cd255583ce388698f14f2da9a31f5ff8e8ad8453
Rebecca Lee <Rebecca Lee>
2016-12-27 16:47:49 -0600

    quiz js formatting

--- quiz.js
+++ quiz.js
@@ -1,28 +1,31 @@
+// Results button action
 $(document).ready(function() 
 {    $("#results").click(function() {                
+	if (!$("input[@name=q1]:checked").val()) {            
+		alert("You're not done yet!");        
+	}        
 
-if (!$("input[@name=q1]:checked").val()) {            
-alert("You're not done yet!");        
-}        
+	else {            
+		var cat1name = "1";            
+		var cat2name = "None";                  
 
-else {            
-var cat1name = "1";            
-var cat2name = "None";            
-            
+		// Check what the user selected
+		var cat1 = ($("input[@name=q1]:checked").val() != "a"); 
+		var cat2 = (!cat1);
+		var categories = [];                        
 
-var cat1 = ($("input[@name=q1]:checked").val() != "a"); 
+		// Push the incorrect question name or "None"
+		if (cat1) { categories.push(cat1name) };           
+		if (cat2) { categories.push(cat2name) };                        
 
-var cat2 = (!cat1); var categories = [];                        
+		var catStr = 'You answered the question incorrectly: ' + categories.join(', ') + ''; 
 
-if (cat1) { categories.push(cat1name) };           
-if (cat2) { categories.push(cat2name) };                        
+		// Display text
+		$("#categorylist").text(catStr);                        
+		$("#categorylist").show("slow");            
 
-var catStr = 'You answered the question incorrectly: ' + categories.join(', ') + '';                     
-$("#categorylist").text(catStr);                        
-$("#categorylist").show("slow");            
-
-if (cat1) { $("#category1").show("slow"); };          
-if (cat2) { $("#category11").show("slow"); };
-{ $("#closing").show("slow"); };
-}
+		if (cat1) { $("#category1").show("slow"); };          
+		if (cat2) { $("#category2").show("slow"); };
+		{ $("#closing").show("slow"); };
+	}
 });});
