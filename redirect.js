038eee26121405fda8e25935706774a3d38c3150
aashimagarg <agarg@utexas.edu>
2016-12-27 16:14:30 -0600

    should redirect to our webpage?

--- redirect.js
+++ redirect.js
@@ -1,13 +1,25 @@
 
-// get the active tab url
-chrome.tabs.getCurrent(function(tab){
-		if (tab.url.search("facebook") != -1) {
-			var newURL = "http://nytimes.com/";
- 			chrome.tabs.create({ url: newURL });
-		}
-        console.log(tab.url);
-    }
-);
+location.replace('url': chrome.extension.getURL('quiztab.html'))
+
+// // get the active tab url
+// chrome.tabs.getCurrent(function(tab){
+// 		// if (tab.url.search("facebook") != -1) {
+// 		// 	var newURL = "http://nytimes.com/";
+//  		// 	chrome.tabs.create({ url: newURL });
+// 		// }
+//         console.log(tab.url);
+//     }
+// );
+
+
+// chrome.webNavigation.onCommitted.addListener(function(e){
+//     if (hasHostSuffix(e.url, 'facebook.com')) {
+//     	console.log(e.url);
+//       	var newURL = "http://nytimes.com/";
+//  		chrome.tabs.create({ url: newURL });
+//  		console.log(tab.url);
+//     }
+// });
 
 // // temporary - when the extension icon is clicked, launch NYTimes
 // chrome.browserAction.onClicked.addListener(function(activeTab){
