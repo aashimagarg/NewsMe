00c1c3d76ad133aaae66a81ec6619bda4aaa227a
Rebecca Lee <Rebecca Lee>
2016-12-28 12:11:33 -0600

    html redirect and fb close tab work

--- quiz.js
+++ quiz.js
@@ -1,24 +1,23 @@
-// Results button action
-$(document).ready(function() 
-{    $("#results").click(function() {                
+$(function() {    //(document).ready for earlier thatn jquery 3.0
+	$("#results").click(function() {                
 	if (!$("input[@name=q1]:checked").val()) {            
 		alert("You're not done yet!");        
 	}        
 
 	else {            
-		var cat1name = "1";            
-		var cat2name = "None";                  
+		var cat1name = "";            
+		var cat2name = "did not ";                  
 
 		// Check what the user selected
 		var cat1 = ($("input[@name=q1]:checked").val() != "a"); 
-		var cat2 = (!cat1);
+		var cat2 = !cat1;
 		var categories = [];                        
 
-		// Push the incorrect question name or "None"
+		// Push the correctness of the answer
 		if (cat1) { categories.push(cat1name) };           
 		if (cat2) { categories.push(cat2name) };                        
 
-		var catStr = 'You answered the question incorrectly: ' + categories.join(', ') + ''; 
+		var catStr = 'You ' + categories[0] + 'answered the question correctly.'; 
 
 		// Display text
 		$("#categorylist").text(catStr);                        
@@ -29,3 +28,76 @@ $(document).ready(function()
 		{ $("#closing").show("slow"); };
 	}
 });});
+
+
+// // scripts here:
+
+// function submitQuiz() {
+// 	//console.log('submitted');
+
+// // get each answer score
+// 	function answerScore (qName) {
+// 		var radiosNo = document.getElementsByName(qName);
+
+// 		for (var i = 0, length = radiosNo.length; i < length; i++) {
+// 				if (radiosNo[i].checked) {
+// 		// do something with radiosNo
+// 				var answerValue = Number(radiosNo[i].value);
+// 			}
+// 		}
+// 		// change NaNs to zero
+// 		if (isNaN(answerValue)) {
+// 			answerValue = 0;
+// 		}
+// 		return answerValue;
+// 	}
+
+// // calc score with answerScore function
+// 	var calcScore = (answerScore('q1') + answerScore('q2') + answerScore('q3') + answerScore('q4'));
+// 	console.log("CalcScore: " + calcScore); // it works!
+
+// // function to return correct answer string
+// 	function correctAnswer (correctStringNo, qNumber) {
+// 		console.log("qNumber: " + qNumber);  // logs 1,2,3,4 after called below
+// 		return ("The correct answer for question #" + qNumber + ": &nbsp;<strong>" +
+// 			(document.getElementById(correctStringNo).innerHTML) + "</strong>");
+// 		}
+
+// // print correct answers only if wrong (calls correctAnswer function)
+// 	if (answerScore('q1') === 0) {
+// 		document.getElementById('correctAnswer1').innerHTML = correctAnswer('correctString1', 1);
+// 	}
+// 	if (answerScore('q2') === 0) {
+// 		document.getElementById('correctAnswer2').innerHTML = correctAnswer('correctString2', 2);
+// 	}
+// 	if (answerScore('q3') === 0) {
+// 		document.getElementById('correctAnswer3').innerHTML = correctAnswer('correctString3', 3);
+// 	}
+// 	if (answerScore('q4') === 0) {
+// 		document.getElementById('correctAnswer4').innerHTML = correctAnswer('correctString4', 4);
+// 	}
+
+// // calculate "possible score" integer
+// 	var questionCountArray = document.getElementsByClassName('question');
+
+// 	var questionCounter = 0;
+// 	for (var i = 0, length = questionCountArray.length; i < length; i++) {
+// 		questionCounter++;
+// 	}
+
+// // show score as "score/possible score"
+// 	var showScore = "Your Score: " + calcScore +"/" + questionCounter;
+// // if 4/4, "perfect score!"
+// 	if (calcScore === questionCounter) {
+// 		showScore = showScore + "&nbsp; <strong>Perfect Score!</strong>"
+// 	};
+// 	document.getElementById('userScore').innerHTML = showScore;
+// }
+
+// $(document).ready(function() {
+
+// 	$('#submitButton').click(function() {
+// 		$(this).addClass('hide');
+// 	});
+
+// });
